c6cf695fc8d7476df2d450ed97494896f2dfddef
angelMarcosCastilla <ajmarcosc15@gmail.com>
2022-02-24 01:42:09 -0500

    add score ui

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,28 @@ function App() {
 
   return (
     <div className="grid">
+      <div className="headerInfo">
+        <div className="turn">
+          <span className="turn-O">O</span>
+          <span className="turn__name">Jugador</span>
+        </div>
+        <div className="turn">
+          <span className="turn-X">X</span>
+          <span className="turn__name">Adversario</span>
+        </div>
+      </div>
       <div className="container">
+        <div className="score">
+          <div className="ronda">
+            <span className="ronda__text">Ronda</span>
+            <span>1/5</span>
+          </div>
+          <div className="puntajes">
+            <p>G: 0</p>
+            <p>P: 0</p>
+            <p>E: 0</p>
+          </div>
+        </div>
         <div className="gridBoard">
           {cells.map((cell, index) => (
             <div
@@ -63,61 +84,41 @@ function App() {
       </div>
       <div className="container-chat">
         <div className="chat">
-          <div className="chat-item chat-item--rigth">
-            Hola como estas 😊
-          </div>
-          <div className="chat-item chat-item--left">
-            Bien  y tu
-          </div>
-          <div className="chat-item chat-item--rigth">
-            Hola como estas 😊
-          </div>
-          <div className="chat-item chat-item--left">
-            Bien  y tu
-          </div>
-          <div className="chat-item chat-item--rigth">
-            Hola como estas 😊
-          </div>
-          <div className="chat-item chat-item--left">
-            Bien  y tu
-          </div>
-          <div className="chat-item chat-item--rigth">
-            Hola como estas 😊
-          </div>
-          <div className="chat-item chat-item--left">
-            Bien  y tu
-          </div>
-          <div className="chat-item chat-item--rigth">
-            Hola como estas 😊
-          </div>
-          <div className="chat-item chat-item--left">
-            Bien  y tu
-          </div>
+          <div className="chat-item chat-item--rigth">Hola como estas 😊</div>
+          <div className="chat-item chat-item--left">Bien y tu</div>
+          <div className="chat-item chat-item--rigth">Hola como estas 😊</div>
+          <div className="chat-item chat-item--left">Bien y tu</div>
+          <div className="chat-item chat-item--rigth">Hola como estas 😊</div>
+          <div className="chat-item chat-item--left">Bien y tu</div>
+          <div className="chat-item chat-item--rigth">Hola como estas 😊</div>
+          <div className="chat-item chat-item--left">Bien y tu</div>
+          <div className="chat-item chat-item--rigth">Hola como estas 😊</div>
+          <div className="chat-item chat-item--left">Bien y tu</div>
         </div>
         <div className="input-group">
           <button className="btn-circle">
-          <svg
-            width="14px"
-            height="19px"
-            viewBox="0 0 14 19"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g
-              id="Page-1"
-              stroke="none"
-              stroke-width="1"
-              fill="none"
-              fill-rule="evenodd"
+            <svg
+              width="14px"
+              height="19px"
+              viewBox="0 0 14 19"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <path
-                d="M7,12 C8.66,12 9.99,10.66 9.99,9 L10,3 C10,1.34 8.66,0 7,0 C5.34,0 4,1.34 4,3 L4,9 C4,10.66 5.34,12 7,12 Z M12.3,9 C12.3,12 9.76,14.1 7,14.1 C4.24,14.1 1.7,12 1.7,9 L0,9 C0,12.41 2.72,15.23 6,15.72 L6,19 L8,19 L8,15.72 C11.28,15.24 14,12.42 14,9 L12.3,9 Z"
-                id="mic"
-                fill="blue"
-                fill-rule="nonzero"
-              ></path>{" "}
-            </g>{" "}
-          </svg>
+              <g
+                id="Page-1"
+                stroke="none"
+                stroke-width="1"
+                fill="none"
+                fill-rule="evenodd"
+              >
+                <path
+                  d="M7,12 C8.66,12 9.99,10.66 9.99,9 L10,3 C10,1.34 8.66,0 7,0 C5.34,0 4,1.34 4,3 L4,9 C4,10.66 5.34,12 7,12 Z M12.3,9 C12.3,12 9.76,14.1 7,14.1 C4.24,14.1 1.7,12 1.7,9 L0,9 C0,12.41 2.72,15.23 6,15.72 L6,19 L8,19 L8,15.72 C11.28,15.24 14,12.42 14,9 L12.3,9 Z"
+                  id="mic"
+                  fill="blue"
+                  fill-rule="nonzero"
+                ></path>{" "}
+              </g>{" "}
+            </svg>
           </button>
           <input type="text" className="input" />
           <button className="button">Enviar</button>
